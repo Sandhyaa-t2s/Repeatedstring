29b612d401e79a2f554e530e9f5769e15db67a25
Sandhyaa-t2s <39513997+Sandhyaa-t2s@users.noreply.github.com>
2018-09-04 09:54:53 +0100

    Add files via upload

---
+++ find.js
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<string.h>
+char *str(const char *s, char *old, char *new1)
+  char *a;
+    int i, count = 0;
+    int newstr = strlen(new1);
+    int oldstr = strlen(old);
+ 
+{
+{
+  int a=i[10],j[10];
+    for (i = 0; s[i] != '\0'; i++)    
+    {
+        if (str(&s[i], old) == &s[i]) 
+        {
+            count++;
+            i += oldstr - 1;
+        }
+    }
+     temp=s[0];
+    }
+    
+}
+int main()
+{
+      char str[100], c[10], d[10];
+        printf("Enter a string:\n");
+
+}
+
